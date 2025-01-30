9f00976192f4645bebc39854b45ad751fa9792f4
n-github2005 <kadudulanamitha@gmail.com>
2025-01-30 22:21:00 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+    public static void main(String[] args) {
+        String str="Hello World";
+        String substring=str.substring(5);
+        System.out.println("Substring:"+substring);
+    }
+    
+}
